6dafd5a494ed87e8fd4d3db09cbccd831454487c
Deepak Singh <deepak.s.ashta@gmail.com>
2023-11-17 12:43:44 +0530

    updated

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,13 @@
+import Chai  from "./chai"
 
 function App() {
 
   return (
-   <h1 >Hello world form vite | Deepak Singh</h1>
+    // <> </> this tag is known as fragment tag we can write div tag too but its is very common problem in react so they provided this tagto make it easy
+    <>  
+      <Chai />
+      <h1 >Hello world form vite | Deepak Singh</h1>
+    </>
   )
 }
 
