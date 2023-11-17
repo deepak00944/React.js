62d039829d03a8ca93236006f0a5852514c7f4df
Deepak Singh <deepak.s.ashta@gmail.com>
2023-11-17 12:43:34 +0530

    updated

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,13 @@
+import Chai from "./Chai"
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+    <Chai/>
+    <h1>Today's Learning</h1>
+    <p>React m javascript file ko mention karne ki need nhi hoti kyuki <u>Typescript</u> use behind the scene add kar deta hlekin vite library m hum khudse add karte h matlab ki vo add hota h </p>
+    <p> We have also seen that how we can add more components abd what errors we can expect from the beginners (need to start the function name in capital letters and in react native have to use <b>".jsx"</b> extensions  ) </p>
+    </>
   );
 }
 
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h3>Ye loo ji react se bhi </h3>
+    )
+}
+
+export default Chai
