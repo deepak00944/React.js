d9b1059ba4edfda968091a9fc3c0565eff51f155
Deepak Singh <deepak.s.ashta@gmail.com>
2023-11-19 11:09:49 +0530

    01vitereact updated

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,14 @@
 import Chai  from "./chai"
 
 function App() {
+  const username = "Deepak Singh"
 
   return (
     // <> </> this tag is known as fragment tag we can write div tag too but its is very common problem in react so they provided this tagto make it easy
+    //yha jo username(ise evaluated expression khete h) h vo js ka h matlab humne uppar jo variable inject kiya h vo hum jsx m sirf evaluated cheeje likhte h matlab ki vha hum or koiu code nhi likh sakte h final outcom likhna h sirf
     <>  
       <Chai />
-      <h1 >Hello world form vite | Deepak Singh</h1>
+      <h1 >Hello world form vite | {username}</h1> 
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+// const reactElement = {
+//   type: "a",
+//   props:{
+//       href: "https://google.com",
+//       target:'_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+
+//react mai khud ka ek syntax hota h element create karne ka 
+//jo variable hum inject karte h vo yha par tree banne ke baad inject hote h nichee tohh dekho
+
+const anotherUser =  "Deepak"
+const reactElement = React.createElement(
+  'a',
+  {href:"https://google.com", target:"_blank"},
+  'Click me to visit google',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    reactElement
+
 )
