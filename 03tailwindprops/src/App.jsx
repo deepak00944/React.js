88725cc023efd3ca2b033c6822fbc50e1592a721
Deepak Singh <deepak.s.ashta@gmail.com>
2023-11-21 12:47:45 +0530

    03tailwind

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,33 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const myObj= {
+    username:"lavanaya",
+    age:21
+  } // we can pass this obj in fragmnets by using curly braces({}, obj = {myObj})
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+   <h1 className='bg-green-400 text-white p-5 rounded-xl' >Tailwind</h1>
+    <Card username="Lisha" btnText = "Visit Me" />
+    <Card username="Navisha" />
+   </>
   )
 }
 
