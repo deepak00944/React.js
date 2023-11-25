ac368d3d64ec632b784c4de9b70550c4a79da9cd
Deepak Singh <deepak.s.ashta@gmail.com>
2023-11-25 11:35:47 +0530

    05passwordGenerator

--- 05passGenerator/src/App.jsx
+++ 05passGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect , useRef} from 'react'
 import './App.css'
 
 function App() {
@@ -7,21 +7,38 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false) ;
   const [password, setPassword] = useState("")
 
+  // useRef hook
+  const passwordRef = useRef(null)
+
+
+  // useCallback hook -- to optimize
   const passwordGenerator = useCallback(() => {
-    const pass = ""
-    const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numberAllowed) str += "0123456789";
     if(charAllowed) str += "!@#$%^&*(){}[]`~_-?"
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random()* str.length + 1)
-      
+      pass += str.charAt(char)
     }
 
     setPassword(pass)
 
 
   },[length,numberAllowed,charAllowed,setPassword])
+
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,100) // only to understand that it is possible to give range
+    window.navigator.clipboard.writeText(password) //kyuki ye core react h toh hume window ka access hota h lekin next js wagera mai nhi kyuuki vo sever par update hoti H
+  },[password])
+  
+
+  // useEffect hook -- page load hone par ye kaam karta h yaa fir dependencies change hone par
+  useEffect(()=>{
+    passwordGenerator()
+  },[charAllowed,numberAllowed,length])
 
   return (
    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
@@ -33,9 +50,49 @@ function App() {
         className='outline-none w-full py-1 px-3'
         placeholder='Password'
         readOnly
+        ref={passwordRef}
       />
-      <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
-
+      <button
+        onClick={copyPasswordToClipboard} 
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:scale-110 hover:bg-black'
+        >Copy
+      </button>
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+          type="range"
+          min={8}
+          max={100}
+          value={length}
+          className='cursor-pointer '
+          onChange={(e)=>{setLength(e.target.value)}}
+        />
+         <label> Length:{length}</label>
+      </div>
+      <div className='flex item-center gap-x-1'>
+        <input 
+          type="checkbox" 
+          defaultChecked={numberAllowed}
+          id="numberInput" 
+          onChange={() => {
+            setNumberAllowed((prev)=>!prev)
+          }}
+        />
+        <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className='flex item-center gap-x-1'>
+        <input 
+          type="checkbox" 
+          defaultChecked={charAllowed}
+          id="charInput" 
+          onChange={() => {
+            setCharAllowed((prev)=>!prev)
+            
+          }}
+        />
+        <label htmlFor="charInput">Character</label>
+      </div>
     </div>
    </div>
   )
