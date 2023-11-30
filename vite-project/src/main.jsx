25050ba1936253475f0907614b5fd0f7bb1cc1cb
Deepak Singh <deepak.s.ashta@gmail.com>
2023-11-30 18:54:08 +0530

    vite-project

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Header from './components/Header/Header.jsx'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
